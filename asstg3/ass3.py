5dde8176059aca51f75272de11d1f251e4ab8d48
S-odland <49540258+S-odland@users.noreply.github.com>
2020-11-04 14:14:23 -0600

    I think this is all the code I need, now onto running the simmulation in coppelia sim

--- asstg3/ass3.py
+++ asstg3/ass3.py
@@ -1,4 +1,4 @@
-import core
+import modern_robotics as mr
 import numpy as np
 
 
@@ -29,7 +29,37 @@ Slist = [[0,         0,         0,         0,        0,        0],
 ## my own code from here on out
 
 g = [0,0,-9.81]
+taulist = [0,0,0,0,0,0]
+Ftip = [0,0,0,0,0,0]
 
 ## All we need to use is the mr.forwardynamics function to find thetadd
 ## taulist is going to be equal to 0
 ## ftip is also going to be 0
+
+## for part 1
+thetalist = np.array([0,0,0,0,0,0])
+dthetalist = np.array([0,0,0,0,0,0])
+
+## for part 2
+thetalist = np.array([-1,-1,0,0,0,0])
+dthetalist = np.array([0,0,0,0,0,0])
+
+############## core.ForwardDynamics --> inputs needed are: #################
+##
+## thetalist -> n list of joint angles
+## dthetalist -> n list of joint velocities
+## taulist --> n list of required joint forces/troques (will be and remain zero for our case)
+## g --> gravity in x,y and z
+## Ftip --> wrench applied to the end effector frame (will be and remain zero for our case)
+## Mlist --> list of link frames i relative to i-1 at the home position
+## Glist --> spatial intertia matrices of the links (will remain the same throughout)
+## Slist --> screw axis of the joints in a space frame
+
+thetalist_iter = np.array(thetalist.copy())
+
+while 1:
+    ddthetalist = mr.ForwardDynamics(thetalist, dthetalist, taulist, g, Ftip, Mlist, Glist, Slist)
+    [thetalist,dthetalist] = mr.EulerStep(thetalist,dthetalist,ddthetalist,0.01)
+    thetalist_iter = np.vstack([thetalist_iter,thetalist])
+
+    
